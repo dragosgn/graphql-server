96fe8ca409603a9520a8c961c6531a39497cd83f
Dragos Nedelcu <dragos.nedelcu@misterspex.de>
2020-03-28 16:00:31 +0100

    fixings

--- resolvers/index.js
+++ resolvers/index.js
@@ -5,23 +5,22 @@ export default {
     users: (parent, args, { models }) => {
       return Object.values(models.users);
     },
-    user: (parent, { id }) => {
-      return users[id];
+    user: (parent, { id }, { models }) => {
+      return models.users[id];
     },
     me: (parent, args, { me }) => {
       return me;
     },
-    messages: () => {
-      return Object.values(messages);
+    message: (parent, { id }, { models }) => {
+      return models.messages[id];
     },
-    message: (parent, { id }) => {
-      return messages[id];
+    messages: (parent, args, { models }) => {
+      return Object.values(models.messages);
     },
   },
   User: {
-    username: (user) => user.username,
-    messages: (user) => {
-      return Object.values(messages).filter(
+    messages: (user, args, { models }) => {
+      return Object.values(models.messages).filter(
         (message) => message.userId === user.id,
       );
     },
@@ -32,23 +31,25 @@ export default {
     },
   },
   Mutation: {
-    createMessage: (parent, { text }, { me }) => {
+    createMessage: (parent, { text }, { me, models }) => {
       const id = uuidv4();
       const message = {
         id,
         text,
         userId: me.id,
       };
-      messages[id] = message;
-      users[me.id].messageIds.push(id);
+
+      models.messages[id] = message;
+      models.users[me.id].messageIds.push(id);
+
       return message;
     },
-    deleteMessage: (parent, { id }) => {
-      const { [id]: message, ...otherMessages } = messages;
+    deleteMessage: (parent, { id }, { models }) => {
+      const { [id]: message, ...otherMessages } = models.messages;
       if (!message) {
         return false;
       }
-      messages = otherMessages;
+      models.messages = otherMessages;
       return true;
     },
   },
