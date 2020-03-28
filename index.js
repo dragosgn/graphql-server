017add8c2dba812b646d6a5131f00875fc41d04b
Dragos Nedelcu <dragos.nedelcu@misterspex.de>
2020-03-28 14:25:02 +0100

    adding apollo

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-import "dotenv/config";
-import cors from "cors";
-import express from "express";
+import express from 'express';
+import { ApolloServer, gql } from 'apollo-server-express';
+
 const app = express();
-app.use(cors());
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const schema = gql`
+  type Query {
+    me: User
+  }
+  type User {
+    username: String!
+  }
+`;
+const resolvers = {
+  Query: {
+    me: () => {
+      return {
+        username: 'Dragos',
+      };
+    },
+  },
+};
+const server = new ApolloServer({
+  typeDefs: schema,
+  resolvers,
 });
-app.listen(process.env.PORT, () =>
-  console.log(`Example app listening on port ${process.env.PORT}!`)
-);
+
+server.applyMiddleware({ app, path: '/graphql' });
+
+app.listen({ port: 8000 }, () => {
+  console.log('Apollo Server on http://localhost:8000/graphql');
+});
